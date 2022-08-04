349dd0ed54629522c1ed566a2ded5c0604fdbe0c
Ben McIntyre <ben@bens-mbp.attlocal.net>
2022-08-04 05:00:50 -0700

    Finished

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
+let content = document.querySelector(".js-generated.content");
 
-let content = document.querySelector('.js-generated.content')
+let header = document.createElement("h1");
+header.setAttribute("class", "dog-name");
+header.append("Rizzo");
+content.append(header);
 
-let header = document.createElement('h1')
-header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
-content.append(header)
+//Correct^
 
-let dogContent = document.createElement('div')
-dogContent.setAttribute('class', 'dog-content')
-content.append(dogContent)
+let dogContent = document.createElement("div");
+dogContent.setAttribute("class", "dog-content");
+content.append(dogContent);
 
-let dogImage = document.createElement('img')
-dogImage.setAttribute('class', 'dog-image')
-dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+let dogImage = document.createElement("img");
+dogImage.setAttribute("class", "dog-image");
+dogImage.setAttribute("src", "./assets/rizzo.jpg");
+dogContent.append(dogImage);
 
-let dogDetails = document.createElement('div')
-dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+//Error^
 
+let dogDetails = document.createElement("div");
+dogDetails.setAttribute("class", "dog-details");
+dogContent.append(dogDetails);
 
+let header1 = document.createElement("h3");
+header1.append("Description:");
+dogDetails.append(header1);
+
+let paragraph1 = document.createElement("p");
+paragraph1.append(
+  "This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals."
+);
+dogDetails.append(paragraph1);
+
+let header2 = document.createElement("h3");
+header2.append("Feeding Times:");
+dogDetails.append(header2);
+
+let feedingList = document.createElement("ul");
+let feed9am = document.createElement("li");
+let feed12pm = document.createElement("li");
+let feed5pm = document.createElement("li");
+feed9am.append("9:00 am");
+feed12pm.append("12:00 pm");
+feed5pm.append("5:00 pm");
+dogDetails.append(feed9am);
+dogDetails.append(feed12pm);
+dogDetails.append(feed5pm);
+dogDetails.append(feedingList);
+
+//Correct^
